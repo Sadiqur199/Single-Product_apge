41d7be2fd8aaac7bf762dcb495680b07fde794c7
sadiq <sadiqurrhaman199@gmail.com>
2024-12-18 09:08:28 +0600

    added hook and ready dummy data

--- src/Component/ProductCard/ProductCard.jsx
+++ src/Component/ProductCard/ProductCard.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductCard = () => {
+  const ProductCard =({onAddToCart}) =>{
+    const [selectedColor, setSelectedColor] = useState("purple");
+    const [selectedSize, setSelectedSize] = useState("")
+  
+  const product ={
+    name : "Classy Modren Smart Watch",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident tempora exercitationem molestias impedit voluptatibus esse officiis vero perspiciatis dolorem ea?",
+    Price: $100,
+    originalPrice: $90,
+    colors:["purple","black","blue","cyan"],
+    sizes:["S","M","XL","XXL"],
+    imageUrls:{
+      purple: "https://i.ibb.co.com/Sxyk4NG/product-gallery.png",
+      black: "https://i.ibb.co.com/HYFnhdv/lg-a-3.png",
+      blue: "https://i.ibb.co.com/YWZQK87/blue.png",
+      cyan: "https://i.ibb.co.com/F3J2NcV/cyan.png",
+    }
+  }
+
   return (
     <div>
       
